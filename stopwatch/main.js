6575fd3b17cd8be0689669d79dc3b98ef5383dd2
Aleksandr <11nav09alex1mo@gmail.com>
2018-09-13 23:10:58 +0300

    edit 13.09.2018 23:10 stopwatch

--- stopwatch/main.js
+++ stopwatch/main.js
@@ -8,57 +8,62 @@ let stop = document.getElementById("control").children[3];
 
 let iRotate;
 let iSeconds;
-let timerEnd;
+let iNextRotate;
+let timerEnd = 0;
 
 start.addEventListener("click", e => {
-  let i = 0;
-  iRotate = setInterval(() => {
-    rotate.textContent = ++i;
+  if (timerEnd == 0) {
+    start.firstElementChild.style.background = "red";
+    let i = 0;
     startClock();
-  }, 1000 * 60 + 25);
-  startClock();
+    iRotate = setInterval(() => {
+      rotate.textContent = ++i;
+      startClock();
+    }, 1000 * 60);
+  }
 });
-
+///////////////////////////////
 let reset = 0;
 stop.addEventListener("click", e => {
   reset++;
-  console.log(reset);
-  if (reset > 1) {
-    pointer.removeAttribute("style");
+  if (reset === 2) {
+    stopClock();
+    rotate.textContent = 0;
     timerEnd = 0;
     seconds.textContent = timerEnd;
-    return;
+    stop.firstElementChild.removeAttribute("style");
+  } else {
+    stop.firstElementChild.style.background = "red";
+    stopClock();
+    pointer.style.transform = `rotate(${timerEnd / (60 / 360)}deg)`;
   }
+
   setTimeout(() => {
     reset = 0;
   }, 500);
-  stopClock();
+
+  start.firstElementChild.removeAttribute("style");
+  clearInterval(iNextRotate);
   clearInterval(iRotate);
-  clearInterval(iSeconds);
-  let deg = timerEnd / (60 / 360);
-  pointer.style.transform = `rotate(${deg}deg)`;
 });
 
 function startClock() {
-  if (timerEnd > 0) {
-    return;
-  }
-  pointer.removeAttribute("style");
   let timer = Date.now();
   pointer.classList.remove("stop");
   pointer.classList.add("active");
   iSeconds = setInterval(() => {
     timerEnd = (Date.now() - timer) / 1000;
-    timerEnd = parseFloat(timerEnd.toFixed(3));
+    timerEnd = timerEnd.toFixed(3);
     seconds.textContent = timerEnd;
   }, 25);
-  setTimeout(() => {
+  iNextRotate = setTimeout(() => {
     stopClock();
-    timer = Date.now();
-  }, 1000 * 60);
+  }, 1000 * 60 - 25);
 }
 
 function stopClock() {
+  clearInterval(iSeconds);
+  pointer.removeAttribute("style");
   pointer.classList.remove("active");
   pointer.classList.add("stop");
 }
