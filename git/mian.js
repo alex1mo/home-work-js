2cb1ccb5ea9341479641be335cab8d35132eabe1
Aleksandr <11nav09alex1mo@gmail.com>
2018-09-21 11:25:43 +0300

    old git

--- git/mian.js
+++ git/mian.js
@@ -1,12 +1,24 @@
 let command = [
-  `git commit -m ""`,
-  `git branch`,
-  `git checkout`,
-  `git merge`,
-  `clear`
+  `$&nbsp;git&nbsp;commit&nbsp;-m&nbsp;"`,
+  `$&nbsp;git&nbsp;branch`,
+  `$&nbsp;git&nbsp;checkout`,
+  `$&nbsp;git&nbsp;merge`,
+  `$&nbsp;clear`
 ];
 
-let target;
+let outputC = [
+  `добавлен commit "`,
+  `добавлена branch "`,
+  `выполнен checkout "`,
+  `выполнен merge "`
+];
+
+let buffer;
+let branch = {};
+let counterB = 0;
+let target = null;
+let targetB = null;
+
 document.getElementById("console").addEventListener("click", function(e) {
   target = document.querySelector("#command").firstElementChild;
   window.addEventListener("keypress", addSymbol);
@@ -46,15 +58,117 @@ function addString(e) {
         );
       }
     } else if (e.keyCode === 13) {
+      output(command, outputC);
+      if (target.innerHTML.indexOf(command[0]) >= 0) {
+        addCommit();
+      } else if (target.innerHTML.indexOf(command[1]) >= 0) {
+        addBrach(buffer);
+      } else if (target.innerHTML.indexOf(command[2]) >= 0) {
+        checkout();
+      }
       target.innerHTML = target.innerHTML.slice(
         target.innerHTML.indexOf("$&nbsp;") + 1,
-        target.innerHTML.length - 1
+        target.innerHTML.length
       );
       document.getElementById("history").innerHTML += `<div>${
         target.innerHTML
       }</div>`;
-
       target.innerHTML = "$&nbsp;";
     }
   }
 }
+
+function output(arr1, arr2) {
+  for (var i = 0; i < arr1.length; i++) {
+    if (target.innerHTML.indexOf(arr1[i]) >= 0) {
+      if (i === 0) {
+        buffer = target.innerHTML.slice(
+          arr(target.innerHTML, arr1[i]) + 1 - "&nbsp;".length,
+          target.innerHTML.length - 1
+        );
+      } else {
+        buffer = target.innerHTML.slice(arr(target.innerHTML, arr1[i]) + 1);
+      }
+      document.querySelector("#result").innerHTML += `<div>${arr2[i] +
+        buffer}"</div>`;
+      break;
+    }
+  }
+}
+
+function arr(arr, index) {
+  let str = "";
+  for (let i = 0; i < arr.length; ++i) {
+    str += arr[i];
+    if (str === index) {
+      return i + "&nbsp;".length;
+    }
+  }
+}
+
+function addCommit() {
+  let div;
+  if (targetB.childElementCount !== 0) {
+    div = document.createElement("div");
+    div.style.cssText += `
+    background-color:${targetB.getAttribute("background-color")};
+    `;
+    targetB.appendChild(div);
+  }
+  div = document.createElement("div");
+  div.style.cssText += `
+  background-color: ${targetB.getAttribute("background-color")};
+  `;
+  targetB.appendChild(div);
+  branch[targetB.getAttribute("name")].commit++;
+}
+
+////////////////////////////////////////////////////////////
+let coordinates = [];
+function addBrach(name) {
+  let y;
+  let x;
+  let color = function() {
+    return parseInt(Math.random() * 200);
+  };
+  let div = document.createElement("div");
+  div.setAttribute("name", name);
+  div.setAttribute("background-color", `rgb(${color()},${color()},${color()})`);
+  div.style.cssText += `
+      top: 0px;
+      left: 0px;
+    `;
+  if (targetB && targetB.lastElementChild) {
+    y = parseFloat(targetB.style.top);
+    x = parseFloat(targetB.lastElementChild.getBoundingClientRect().x);
+    coordinates[0] = targetB.lastElementChild.getBoundingClientRect();
+    div.style.cssText += `
+      top: ${(y + 30) * counterB}px;
+      left: ${x + 30}px;
+    `;
+    branch[targetB.getAttribute("name")].branch++;
+  }
+  branch[name] = {
+    name: name,
+    commit: 0,
+    branch: 1
+  };
+  document.querySelector("#branch").appendChild(div);
+  counterB++;
+  if (document.querySelector("#branch").lastElementChild.lastElementChild) {
+    debugger;
+    coordinates[1] = document
+      .querySelector("#branch")
+      .lastElementChild.lastElementChild.getBoundingClientRect();
+    let x1 = coordinates[0].x;
+    let y1 = coordinates[0].y;
+    let x2 = coordinates[1].x;
+    let y2 = coordinates[1].y;
+  }
+}
+
+function checkout() {
+  targetB = document.querySelector(`[name=${buffer}]`);
+}
+
+// document.body.getBoundingClientRect
